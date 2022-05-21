4a5ff18f5079088edb21e810332b8e2297521681
Blessing <bigbadumhe@gmail.com>
2022-05-21 13:33:11 -0500

    completed work

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
 const cTable = require("console.table");
+const { query } = require("./db/connection");
 
 function viewAllDepartment() {
   const sql = `SELECT * FROM department`;
@@ -17,10 +18,151 @@ function viewAllRoles() {
 }
 
 function viewAllEmployee() {
-  const sql3 = `SELECT e.id AS "Employee ID", e.first_name AS "First Name", e.last_name AS "LAST NAME", roles.title AS Title, roles.salary AS Salary, department.name AS "Department Name", m.first_name AS "Employees Manager" From employee e JOIN roles ON e.role_id = roles.id JOIN department ON roles.department_id = department.id LEFT JOIN employee m ON m.id = e.manager_id;`
+  const sql3 = `SELECT e.id AS "Employee ID", e.first_name AS "First Name", e.last_name AS "LAST NAME", roles.title AS Title, roles.salary AS Salary, department.name AS "Department Name", m.first_name AS "Employees Manager" From employee e JOIN roles ON e.role_id = roles.id JOIN department ON roles.department_id = department.id LEFT JOIN employee m ON m.id = e.manager_id;`;
   db.query(sql3, (err, rows) => {
     console.table(rows);
   });
+}
+
+function updatedEmployeeRole() {
+  let sql = `SELECT employee.id, employee.first_name, employee.last_name, roles.id AS "role_id"                   FROM employee, roles, department WHERE department.id = roles.department_id AND roles.id = employee.role_id`;
+
+  db.query(sql, (error, response) => {
+    if (error) throw error;
+
+    let employeeNamesArray = [];
+    response.forEach((employee) => {
+      employeeNamesArray.push(`${employee.first_name} ${employee.last_name}`);
+    });
+
+    let rolesql = `SELECT roles.id, roles.title FROM roles`;
+    db.query(rolesql, (error, response) => {
+      if (error) throw error;
+      let rolesArray = [];
+      response.forEach((role) => {
+        rolesArray.push(role.title);
+      });
+
+      inquirer
+        .prompt([
+          {
+            name: "chosenEmployee",
+            type: "list",
+            message: "Which employee has a new role?",
+            choices: employeeNamesArray,
+          },
+          {
+            name: "chosenRole",
+            type: "list",
+            message: "What is their new role?",
+            choices: rolesArray,
+          },
+        ])
+        .then((answer) => {
+          let newTitleId, employeeId;
+
+          response.forEach((role) => {
+            if (answer.chosenRole === role.title) {
+              newTitleId = role.id;
+            }
+          });
+
+          response.forEach((employee) => {
+            if (
+              answer.chosenEmployee ===
+              `${employee.first_name} ${employee.last_name}`
+            ) {
+              employeeId = employee.id;
+            }
+          });
+
+          let employeesqls = `UPDATE employee SET employee.role_id = ? WHERE employee.id = ?`;
+
+          db.query(
+            employeesqls,
+            [newTitleId, employeeId],
+
+            (error) => {
+              if (error) throw error;
+              promptUser()
+            }
+
+          )
+
+
+
+        });
+    });
+  });
+}
+
+function addedEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "what is the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employee's last name?",
+      },
+    ])
+    .then((answer) => {
+      const nameInformation = [answer.firstName, answer.lastName];
+      const rolessql = `SELECT roles.id, roles.title FROM roles`;
+      db.query(rolessql, (error, data) => {
+        if (error) throw error;
+        const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "role",
+              message: "What is the employee's role?",
+              choices: roles,
+            },
+          ])
+          .then((rolechoice) => {
+            const role = rolechoice.role;
+            nameInformation.push(role);
+
+            const managersql = `SELECT * FROM employee`;
+            db.query(managersql, (error, data) => {
+              if (error) throw error;
+              const managers = data.map(({ id, first_name, last_name }) => ({
+                name: first_name + " " + last_name,
+                value: id,
+              }));
+
+              inquirer
+                .prompt([
+                  {
+                    type: "list",
+                    name: "manager",
+                    message: "Who is the employee's manager?",
+                    choices: managers,
+                  },
+                ])
+                .then((managerChoice) => {
+                  const manager = managerChoice.manager;
+                  nameInformation.push(manager);
+
+                  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                    VALUES (?, ?, ?, ?)`;
+                  db.query(sql, nameInformation, (error) => {
+                    if (error) throw error;
+                    console.log("Employee has been added");
+                    viewAllEmployee();
+                  });
+                });
+            });
+          });
+      });
+    });
 }
 
 function addedDeparment() {
@@ -88,7 +230,7 @@ function addedRoles() {
       ])
       .then((answer) => {
         if (answer.departmentName === "Add new Department") {
-          console.log("You must add the Department First")
+          console.log("You must add the Department First");
           addedDeparment();
         } else {
           addRoleResume(answer);
@@ -151,6 +293,7 @@ const promptUser = () => {
           "add a role",
           "add an employee",
           "update an employee role",
+          "exit"
         ],
       },
     ])
@@ -175,14 +318,13 @@ const promptUser = () => {
           addedRoles();
           break;
         case "add an employee":
-          console.log("add an employee");
+          addedEmployee();
           break;
         case "update an employee role":
-          console.log("shat-up");
+          updatedEmployeeRole();
           break;
-        default:
-          "I may never be used";
-          console.log("who cares");
+        case "exit":
+          db.end()
           break;
       }
     });
